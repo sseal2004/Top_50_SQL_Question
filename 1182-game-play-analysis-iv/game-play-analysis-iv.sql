a8f1927cab7a8a830eb964a5e1dbb14917a64ef8
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-07-08 20:47:44 +0530

    Time: 615 ms (35.16%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,3 +1,8 @@
 select round(count(player_id)/(select count(distinct player_id ) from activity),2) as fraction from Activity where (player_id,date_sub(event_date,interval 1 day)) in (
 select player_id, min(event_date) firstlogin from Activity group by player_id);
 
+-- this means all player id atleast once with no duplication
+-- select count(distinct player_id ) from activity
+-- all player with id and their first login
+-- select player_id, min(event_date) firstlogin from Activity group by player_id
+--  where (player_id,date_sub(event_date,interval 1 day) atleast a player logged in consequtive day

c1506073db058776fe3ddbb63e7e4546d9bd61a1
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-07-08 21:55:29 +0530

    Time: 506 ms (57.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,1 @@
+select activity_date day ,count(distinct user_id) active_users  from Activity where datediff('2019-07-27',activity_date)>=0 and datediff('2019-07-27',activity_date)<30 group by activity_date;
